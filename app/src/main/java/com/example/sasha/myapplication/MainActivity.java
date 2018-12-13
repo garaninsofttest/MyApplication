83bd47ba523c1ba88743e267e1ff19ded0190bfe
Alex Garanin <garaninsoft@gmail.com>
2018-12-13 16:16:18 +0300

    Обязательно здесь написать комент

--- app/src/main/java/com/example/sasha/myapplication/MainActivity.java
+++ app/src/main/java/com/example/sasha/myapplication/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends Activity implements NumberPicker.OnValueChange
     static Dialog d;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //Hello from GitHub
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
